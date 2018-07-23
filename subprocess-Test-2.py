d04cbc4a5111b48af6211b7961b0bd689e951cdb
Вязовой Денис <32854318+indikv81@users.noreply.github.com>
2018-07-23 19:25:39 +0800

    Первая версия

--- subprocess-Test-2.py
+++ subprocess-Test-2.py
@@ -2,10 +2,10 @@ import subprocess as sp
 ip_1 = 10
 ip_2 = 15
 ip_3 = 11
-ip_4 = 111
+ip_4 = 112
 ip = str(ip_1) + "." + str(ip_2) + "." + str(ip_3) + "." + str(ip_4)
 print(ip)
-ip2 = "www.rambler.ru"
+ip2 = "www.yandex.ru"
 output = sp.check_output(["ping", "-n", "1", ip2]).decode('cp866', 'ignore')
 print(output)
 
