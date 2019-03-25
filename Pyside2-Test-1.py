eb7997172bb314362e0e005c124191e906b3f988
Vyazovoy Denis <indikv@list.ru>
2019-03-26 02:21:42 +0300

    26.03.2019

--- Pyside2-Test-1.py
+++ Pyside2-Test-1.py
@@ -1,18 +1,34 @@
 # 26.03.2019
 
 import sys
-from PySide2.QtWidgets import QApplication, QPushButton
-from PySide2.QtCore import Slot
+from PySide2.QtWidgets import (QLineEdit, QPushButton, QApplication,
+    QVBoxLayout, QDialog)
 
-@Slot()
-def say_hello():
- print("Button clicked, Hello!")
+class Form(QDialog):
 
-# Create the Qt Application
-app = QApplication(sys.argv)
-# Create a button, connect it and show it
-button = QPushButton("Click me")
-button.clicked.connect(say_hello)
-button.show()
-# Run the main Qt loop
-app.exec_()
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+        # Create widgets
+        self.edit = QLineEdit("Write my name here")
+        self.button = QPushButton("Show Greetings")
+        # Create layout and add widgets
+        layout = QVBoxLayout()
+        layout.addWidget(self.edit)
+        layout.addWidget(self.button)
+        # Set dialog layout
+        self.setLayout(layout)
+        # Add button signal to greetings slot
+        self.button.clicked.connect(self.greetings)
+
+    # Greets the user
+    def greetings(self):
+        print ("Hello %s" % self.edit.text())
+
+if __name__ == '__main__':
+    # Create the Qt Application
+    app = QApplication(sys.argv)
+    # Create and show the form
+    form = Form()
+    form.show()
+    # Run the main Qt loop
+    sys.exit(app.exec_())
