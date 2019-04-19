db818d2f6f5564833a688aa34ddeb013661bea32
Vyazovoy Denis <indikv@list.ru>
2019-04-19 13:16:23 +0400

    19.04.2019

--- subprocess-Test-2.py
+++ subprocess-Test-2.py
@@ -1,20 +1,26 @@
 import subprocess as sp
-ip_1 = 10
-ip_2 = 15
-ip_3 = 11
-ip_4 = 114
-ip = str(ip_1) + "." + str(ip_2) + "." + str(ip_3) + "." + str(ip_4)
-print(ip)
-ip2 = "www.yandex.ru"
-output = sp.check_output(["ping", "-n", "1", ip2]).decode('cp866', 'ignore')
-print(output)
 
-def ipcheck():
-    status,result = sp.getstatusoutput("ping -n 1 " + str(ip))
+def ipcheck(fnip):
+    print("Тест: " + fnip)
+    status, result = sp.getstatusoutput("ping -n 1 " + str(fnip))
     if status == 0:
-        print("System " + str(ip) + " is UP !")
+        print("System " + str(fnip) + " is UP !")
     else:
-        print("System " + str(ip) + " is DOWN !")
+        print("System " + str(fnip) + " is DOWN !")
     print(status)
 
-ipcheck()
+ip_1 = 192
+ip_2 = 168
+ip_3 = 1
+ip_4 = 109
+ip = str(ip_1) + "." + str(ip_2) + "." + str(ip_3) + "." + str(ip_4)
+print(ip)
+output = sp.check_output(["ping", "-n", "1", ip]).decode('cp866', 'ignore')
+print(output)
+ipcheck(ip)
+
+ip2 = "www.yandex.ru"
+output = sp.check_output(["ping", "-n", "1", ip2]).decode('cp866', 'ignore')
+print(ip2)
+print(output)
+ipcheck(ip2)
